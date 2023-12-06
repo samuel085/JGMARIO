8e27417aa512d17aace0412b9264eb3502b01f16
samuel <samuellimazm60@gmail.com>
2023-12-06 15:50:52 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
 const jump = () => {
-    mario.classList.add('jump')
+    mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500
+        mario.classList.remove('jump');
+    }, 500);
+};
 
-    )
-}
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'image/game-over.png-20231123T190252Z-001/game-over.png'
+        mario.style.width = '80px'
+        mario.style.marginLeft = '50px'
 
 
-document.addEventListener ('keydown', jump);
-  
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
